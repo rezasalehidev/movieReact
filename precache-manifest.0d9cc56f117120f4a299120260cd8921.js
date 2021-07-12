8653dfca5878fecdbf0a5fad990c0080deefcae8
rezasalehi <rezasalehidev@gmail.com>
2021-07-12 13:51:08 +0430

    Updates

---
+++ precache-manifest.0d9cc56f117120f4a299120260cd8921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf497943c7b4c9266ad6ef81d5deedf5",
+    "url": "/movieReact/index.html"
+  },
+  {
+    "revision": "4a1fff18d7a4ce8a58ec",
+    "url": "/movieReact/static/css/main.289c1fa7.chunk.css"
+  },
+  {
+    "revision": "9aff709ffce6dd2dd0a7",
+    "url": "/movieReact/static/js/2.5b374e73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieReact/static/js/2.5b374e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a1fff18d7a4ce8a58ec",
+    "url": "/movieReact/static/js/main.44e5b348.chunk.js"
+  },
+  {
+    "revision": "fdc33b3a31e9f0c8d625",
+    "url": "/movieReact/static/js/runtime-main.f8710978.js"
+  }
+]);
